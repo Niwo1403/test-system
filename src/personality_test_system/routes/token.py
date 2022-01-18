98ccf456f9d6fb998564fe10ec25e9aab6884c1a
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-18 22:19:20 +0100

    Adjusted args reading of POST token.

--- src/personality_test_system/routes/token.py
+++ src/personality_test_system/routes/token.py
@@ -7,18 +7,18 @@ from personality_test_system.models import db, Token, User
 
 
 @app.route(f'{API_PREFIX}/token/', methods=['POST'])
-def get_token():
-    username = request.args.get("username", default=None, type=str)
-    password = request.args.get("password", default=None, type=str)
+def post_token():
+    username = request.args.get("username", type=str)
+    password = request.args.get("password", type=str)
+    max_usage_count = request.args.get("max_usage_count", type=int)
+    personality_test_name = request.args.get("personality_test_name", type=str)
+    if personality_test_name is None or username is None or password is None:
+        abort(400)
+
     user = db.session.query(User).filter_by(username=username).first()
     if user is None or not user.validate_password(password):
         abort(401)
     app.logger.info(f"Requested token with username '{username}' and valid password.")
-
-    max_usage_count = request.args.get("max_usage_count", default=None, type=int)
-    personality_test_name = request.args.get("personality_test_name", type=str)
-    if personality_test_name is None:
-        abort(400)
 
     token = Token.generate_token(max_usage_count, personality_test_name)
     db.session.add(token)
