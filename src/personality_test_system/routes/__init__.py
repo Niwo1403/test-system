8641098ee111d569689516b4d44502b6f96c60e1
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-18 22:24:11 +0100

    Added route for personality-test.

--- src/personality_test_system/routes/__init__.py
+++ src/personality_test_system/routes/__init__.py
@@ -1,2 +1,2 @@
 # custom
-from . import root, token  # must be named explicitly, * wouldn't work (intended by internal python design)!
+from . import root, token, personality_test  # must be named explicitly, * wouldn't work (intended by internal python design)!
---
+++ src/personality_test_system/routes/personality_test.py
@@ -0,0 +1,26 @@
+# 3rd party
+from flask import request, abort
+# custom
+from personality_test_system import app
+from personality_test_system.constants import API_PREFIX
+from personality_test_system.models import db, Token, PersonalityTest
+
+
+@app.route(f'{API_PREFIX}/personality-test/', methods=['GET'])
+def get_personality_test():
+    request_token = request.args.get("token", type=str)
+    if request_token is None:
+        abort(400)
+
+    token = db.session.query(Token).filter_by(token=request_token).first()
+    if token is None or token.is_expired():
+        abort(404)
+
+    pers_test = db.session.query(PersonalityTest).filter_by(name=token.personality_test_name).first()
+    if pers_test is None:
+        abort(404)
+
+    app.logger.info(f"Requested personality-test '{pers_test.name}' with token '{token.token}'.")
+
+    return pers_test.description_json
+
