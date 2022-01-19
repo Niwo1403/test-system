db6cc8351137f28b00209a22e21e50e423eafe54
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-19 16:56:02 +0100

    Shortened creation of default database data for development.

--- src/personality_test_system/models/__init__.py
+++ src/personality_test_system/models/__init__.py
@@ -5,17 +5,9 @@ from .database import _create_tables_if_not_exist, db
 _create_tables_if_not_exist()  # must be run after import of models ("from .table_models import *")
 
 # TODO: remove in production:
-if db.session.query(User).first() is None:  # if no user exist, created default one
+if db.session.query(User).first() is None:  # if no user exist, assume database is empty & create development examples
     u = User(username="admin", password="admin")
-    db.session.add(u)
-    db.session.commit()
-
-if db.session.query(PersonalityTest).first() is None:  # if no user exist, created default one
     p = PersonalityTest(name="PersTest", description_json={"Test": True})
-    db.session.add(p)
-    db.session.commit()
-
-if db.session.query(Token).first() is None:  # if no user exist, created default one
     t = Token(token="asdf", personality_test_names=["UnknownTest", "PersTest"], max_usage_count=2)
-    db.session.add(t)
+    db.session.add_all((u, p, t))
     db.session.commit()
