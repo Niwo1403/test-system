eee4e9a01862ddda5a5c4842510b2f6e97cb1828
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-19 16:58:45 +0100

    Fixed position of person data creation and personality test existence check.

--- src/personality_test_system/routes/personality_test_answer.py
+++ src/personality_test_system/routes/personality_test_answer.py
@@ -17,13 +17,13 @@ def post_personality_test_answer():
     if not all((answer_set, personality_test_name, person_name, person_age, person_gender)):
         abort(400, "Argument missing.")
 
-    person = Person(name=person_name, gender=person_gender, age=person_age, position=person_position)
-    db.session.add(person)
-    db.session.commit()  # required to generate id of person
-
     corresponding_test = db.session.query(PersonalityTest).filter_by(name=personality_test_name).first()
     if corresponding_test is None:
         abort(404, "Personality test doesn't exist.")  # Test not found
+
+    person = Person(name=person_name, gender=person_gender, age=person_age, position=person_position)
+    db.session.add(person)
+    db.session.commit()  # required to generate id of person
 
     answer = PersonalityTestAnswer(date=db.func.now(),
                                    answer_set=answer_set,
