34f4f555fe53ba7e0522906b02b9147bc6d07672
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-20 22:47:02 +0100

    Renamed files of routes.

--- src/personality_test_system/routes/__init__.py
+++ src/personality_test_system/routes/__init__.py
@@ -1,5 +1,5 @@
 # custom
 from . import root, \
     token, \
-    personality_test, \
-    personality_test_answer  # must be named explicitly, * wouldn't work (intended by internal python design)!
+    tests, \
+    test_answer  # must be named explicitly, * wouldn't work (intended by internal python design)!
