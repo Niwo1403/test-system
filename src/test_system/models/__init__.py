09e779341d3e205616271e59bc79548782a711ae
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-27 21:41:03 +0100

    Adjusted models __init__.py file to set more meaningful test data for tables.

--- src/test_system/models/__init__.py
+++ src/test_system/models/__init__.py
@@ -6,14 +6,96 @@ _create_tables_if_not_exist()  # must be run after import of models ("from .tabl
 
 # TODO: remove in production:
 if User.query.first() is None:  # if no user exist, assume database is empty & create development examples
-    u = User(username="admin", password="admin")
-    p = Test(name="PersTest", description_json={"Test": True}, test_category=Test.CATEGORIES.EVALUABLE_TEST)
-    p2 = Test(name="Person", description_json={"Person": True}, test_category=Test.CATEGORIES.PERSONAL_DATA_TEST)
-    p3 = Test(name="PreCol", description_json={"Pre collecting...": True}, test_category=Test.CATEGORIES.PRE_COLLECT_TEST)
-    t = Token(token="asdf",
-              max_usage_count=2,
-              personal_data_test_name="Person",
-              pre_collection_test_names=["UnknownTest", "PersTest"],
-              evaluable_test_name="PersTest")
-    db.session.add_all((u, p, p2, p3, t))
+    default_user = User(username="admin", password="admin")
+    person_test = Test(name="Person", description_json={
+        "title": "Persöhnliche Daten",
+        "logoPosition": "right",
+        "pages": [
+            {
+                "name": "page1",
+                "elements": [
+                    {
+                        "type": "text",
+                        "name": "name",
+                        "title": "Name:",
+                        "isRequired": True
+                    },
+                    {
+                        "type": "text",
+                        "name": "age",
+                        "title": "Alter",
+                        "isRequired": True,
+                        "inputType": "number",
+                        "min": 1,
+                        "max": 200
+                    },
+                    {
+                        "type": "radiogroup",
+                        "name": "gender",
+                        "title": "Geschlecht",
+                        "isRequired": True,
+                        "choices": [
+                            {
+                                "value": "m",
+                                "text": "männlich"
+                            },
+                            {
+                                "value": "w",
+                                "text": "weiblich"
+                            },
+                            {
+                                "value": "s",
+                                "text": "sonstiges"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "text",
+                        "name": "position",
+                        "title": "Position"
+                    }
+                ]
+            }
+        ]
+    }, test_category=Test.CATEGORIES.PERSONAL_DATA_TEST)
+    pre_collect_test = Test(name="PreCol", description_json={
+        "title": "Zusetzliche persöhnliche Daten",
+        "logoPosition": "right",
+        "pages": [
+            {
+                "name": "page1",
+                "elements": [
+                    {
+                        "type": "text",
+                        "name": "word",
+                        "title": "Lieblingswort",
+                        "isRequired": True
+                    }
+                ]
+            }
+        ]
+    }, test_category=Test.CATEGORIES.PRE_COLLECT_TEST)
+    pers_test = Test(name="PersTest", description_json={
+        "title": "Persöhnlichkeitstest",
+        "logoPosition": "right",
+        "pages": [
+            {
+                "name": "page1",
+                "elements": [
+                    {
+                        "type": "text",
+                        "name": "result",
+                        "title": "Lorem ipsum",
+                        "isRequired": True
+                    }
+                ]
+            }
+        ]
+    }, test_category=Test.CATEGORIES.EVALUABLE_TEST)
+    test_token = Token(token="asdf",
+                       max_usage_count=2,
+                       personal_data_test_name="Person",
+                       pre_collection_test_names=["PreCol", "PreCol"],
+                       evaluable_test_name="PersTest")
+    db.session.add_all((default_user, person_test, pre_collect_test, pers_test, test_token))
     db.session.commit()
