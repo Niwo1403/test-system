eeb5f2d9a8b4403755b02497ad1dba9b62ecf83f
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-28 16:48:13 +0100

    Fixed evaluation of conftest.pys directory for relative paths.

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,15 @@
 # std
 from sys import path
-from os.path import join as path_join
+from os.path import join as path_join, realpath, dirname
 from pytest import fixture
 # 3rd party
 from dotenv import load_dotenv
 
-path.append(path_join(path[0], "../src"))  # add src directory to path to import from test_system
-load_dotenv(path_join(path[0], ".env"))  # load test env variables for testing before project imports & code
+FILE_DIR = dirname(realpath(__file__))
+path.append(path_join(FILE_DIR, "../src"))  # add src directory to path to import from test_system
+load_dotenv(path_join(FILE_DIR, ".env"), override=True)  # load test env variables for testing before project imports & code
 
 # custom
-from test_system import app
+from test_system import app  # must not be at top of file
 
 app.config['TESTING'] = True
