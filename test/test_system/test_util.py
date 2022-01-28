41815e8c96e3e403d86a29f90f314abab254ede7
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-28 17:05:51 +0100

    Wrote generate_hash test.

--- test/test_system/test_util.py
+++ test/test_system/test_util.py
@@ -2,5 +2,12 @@
 from test_system import util
 
 
-def test_generate_hash():
-    assert True
+def test_generate_hash(test_var):
+    test_password = "test password"
+    test_username = "test username"
+    generated_salted_hash = util.generate_hash(test_password, test_username)
+    salt, generated_hash = generated_salted_hash.split(util.HASH_SEPARATOR)
+    re_generated_salted_hash = util.generate_hash(test_password, test_username, salt=salt)
+    assert generated_salted_hash is not None, "Got empty or None response."
+    assert len(generated_hash) == 128, "Hash got wrong length."
+    assert generated_salted_hash == re_generated_salted_hash, "Re-generation of hash failed."
