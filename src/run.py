59a25111ad21438afbab1401106514fee3038751
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-28 16:03:21 +0100

    Added host "0.0.0.0" to flasks run, to enable other devices from the same network to access the webserver for testing (e.g. a mobile device).

--- src/run.py
+++ src/run.py
@@ -3,4 +3,4 @@ from test_system import app, PORT
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=PORT)  # threaded=True for multiple user access
+    app.run(threaded=True, port=PORT, host="0.0.0.0")  # threaded=True for multiple user access
