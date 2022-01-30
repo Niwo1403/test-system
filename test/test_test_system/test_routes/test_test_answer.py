d49eb8fbbbcfeea3e8f732a7fe3db8046139b874
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-01-30 14:24:45 +0100

    Added raise_if_insert_in_tables calls to test_answer tests.

--- test/test_test_system/test_routes/test_test_answer.py
+++ test/test_test_system/test_routes/test_test_answer.py
@@ -42,39 +42,43 @@ def test_post_test_answer__with_success(client, session, person, test):
            answer.person_id == person.id, f"TestAnswer written to database, got wrong data: {answer}"
 
 
-def test_post_test_answer__with_bad_request(client, session, person, test):
+def test_post_test_answer__with_bad_request(client, session, raise_if_insert_in_tables, person, test):
     session.add_all((person, test))
     session.commit()
 
-    test_arguments = [{"test-name": test.name}, {"person-id": person.id}]
+    test_arguments = [{"person-id": person.id}]
 
-    for query_string in test_arguments:
-        resp = client.post(ROUTE, data=json_dumps(test_answer), query_string=query_string)
-        assert resp.status_code == 400, f"Got wrong status code at {ROUTE} for arguments: {query_string}"
+    with raise_if_insert_in_tables(Person, Test, TestAnswer):
+        for query_string in test_arguments:
+            resp = client.post(ROUTE, data=json_dumps(test_answer), query_string=query_string)
+            assert resp.status_code == 400, f"Got wrong status code at {ROUTE} for arguments: {query_string}"
 
 
-def test_post_test_answer__with_wrong_test(client, session, person, personal_data_test):
+def test_post_test_answer__with_wrong_test(client, session, raise_if_insert_in_tables, person, personal_data_test):
     session.add_all((person, personal_data_test))
     session.commit()
 
     unknown_test_name = "?`?`?`?`?`?`?`?`?`?`?`?`?`?`?"
-    resp = client.post(ROUTE,
-                       data=json_dumps(test_answer),
-                       query_string={"test-name": unknown_test_name, "person-id": person.id})
-    assert resp.status_code == 404, f"Got wrong status code at {ROUTE} for unknown test name: {unknown_test_name}"
 
-    resp = client.post(ROUTE,
-                       data=json_dumps(test_answer),
-                       query_string={"test-name": personal_data_test.name, "person-id": person.id})
-    assert resp.status_code == 500, (f"Got wrong status code at {ROUTE} for test "
-                                     f"with wrong test category: {personal_data_test.name}")
+    with raise_if_insert_in_tables(Person, Test, TestAnswer):
+        resp = client.post(ROUTE,
+                           data=json_dumps(test_answer),
+                           query_string={"test-name": unknown_test_name, "person-id": person.id})
+        assert resp.status_code == 404, f"Got wrong status code at {ROUTE} for unknown test name: {unknown_test_name}"
+
+        resp = client.post(ROUTE,
+                           data=json_dumps(test_answer),
+                           query_string={"test-name": personal_data_test.name, "person-id": person.id})
+        assert resp.status_code == 500, (f"Got wrong status code at {ROUTE} for test "
+                                         f"with wrong test category: {personal_data_test.name}")
 
 
-def test_post_test_answer__with_wrong_person(client, session, test):
+def test_post_test_answer__with_wrong_person(client, session, raise_if_insert_in_tables, test):
     session.add(test)
     session.commit()
 
-    resp = client.post(ROUTE,
-                       data=json_dumps(test_answer),
-                       query_string={"test-name": test.name, "person-id": -1})
-    assert resp.status_code == 404, f"Got wrong status code at {ROUTE} for unknown person with id: {-1}"
+    with raise_if_insert_in_tables(Person, Test, TestAnswer):
+        resp = client.post(ROUTE,
+                           data=json_dumps(test_answer),
+                           query_string={"test-name": test.name, "person-id": -1})
+        assert resp.status_code == 404, f"Got wrong status code at {ROUTE} for unknown person with id: {-1}"
