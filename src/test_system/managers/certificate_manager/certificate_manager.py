fb7cd084bb81057ab9c75b55eb225e757158351d
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-02-04 14:24:28 +0100

    Added more meaningful pseudo logic to the processing of evaluable test answers in certificate manager.

--- src/test_system/managers/certificate_manager/certificate_manager.py
+++ src/test_system/managers/certificate_manager/certificate_manager.py
@@ -1,13 +1,13 @@
 # custom
 from .pdf import PDF
-from test_system.models import TestAnswer, Person
+from test_system.models import EvaluableTestAnswer, Person, TestAnswer
 
 
 class CertificateManager:
 
-    def __init__(self, test_answer: TestAnswer, person: Person):
+    def __init__(self, evaluable_test_answer: EvaluableTestAnswer, person: Person):
         self.pdf = PDF()
-        self.test_answer = test_answer
+        self.evaluable_test_answer = evaluable_test_answer
         self.person = person
 
     def generate_certificate(self) -> bytes:
@@ -24,6 +24,8 @@ class CertificateManager:
             self.pdf.add_default_cell(f'Position: {self.person.position}')
 
     def _add_answer_text(self):
-        self.pdf.add_default_cell(f'Test abgegeben am: {self.test_answer.date}')
-        self.pdf.add_default_cell(f'Test Name: {self.test_answer.test_name}')
-        self.pdf.add_default_cell(f'Test Antworten: {self.test_answer.answer_set}')
+        test_answer_id = self.evaluable_test_answer.test_answer_id
+        test_answer = TestAnswer.query.filter_by(id=test_answer_id)
+        self.pdf.add_default_cell(f'Test abgegeben am: {test_answer.date}')
+        self.pdf.add_default_cell(f'Test Name: {test_answer.test_name}')
+        self.pdf.add_default_cell(f'Test Antworten: {test_answer.answer_set}')
