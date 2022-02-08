7f3f7914b5a0e2025f2a4279e54bd966405e46a1
Niwo1403 <nicolai-wolfrom@gmx.de>
2022-02-08 17:57:23 +0100

    Adjusted person test to test for all mandatory attributes and JSON format validation.

--- test/test_test_system/test_routes/test_person.py
+++ test/test_test_system/test_routes/test_person.py
@@ -31,14 +31,20 @@ def test_post_person__with_success(client: FlaskClient):
 def test_post_person__with_bad_request(client: FlaskClient,
                                        raise_if_change_in_tables: Callable[[db.Model], ContextManager]):
     test_cases = {
-        "data with missing attr": {"name": "Max M.", "age": 18, "position": "POS"},
+        "data with no name": {"age": 18, "gender": "s", "position": "POS"},
+        "data with no age": {"name": "", "gender": "s", "position": "POS"},
+        "data with no gender": {"name": "", "age": 18, "position": "POS"},
         "data with age out of range": {"name": "Max M.", "age": -1, "gender": "s", "position": "POS"},
         "data with unknown gender": {"name": "Max M.", "age": 18, "gender": "UNKNOWN", "position": "POS"},
-        "data with age wrong type": {"name": "Max M.", "age": "18", "gender": "s", "position": "POS"},
-        "data with name empty": {"name": "", "age": 18, "gender": "s", "position": "POS"}
+        "data with age wrong type": {"name": "Max M.", "age": "18", "gender": "s", "position": "POS"}
     }
+    for k, v in test_cases.items():
+        test_cases[k] = json_dumps(v)
+    test_cases.update({
+        "data with wrong json (missing })": '{"name": "Max M.", "age": 20, "gender": "s", "position": "POS" ',
+        "data with wrong json (wrong key)": '{"name": "Max M.", "age": 20, "gender": "s", null: "POS"}'})
 
     with raise_if_change_in_tables(Person):
         for data_name, test_data in test_cases.items():
-            resp = client.post(ROUTE, data=json_dumps(test_data))
+            resp = client.post(ROUTE, data=test_data)
             assert resp.status_code == 400, f"Got wrong status code at {ROUTE} for {data_name}: {test_data}"
